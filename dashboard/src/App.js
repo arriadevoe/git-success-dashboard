3b4f91519911a53188792f96bf91b55f1fe79843
sapinspys <garri009@fiu.edu>
2020-07-30 15:44:18 -0400

    Implemented ant design and laid out dashboard

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Layout } from "antd";
+import "./App.less";
 
-function App() {
+const { Sider, Content } = Layout;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="screen">
+      <Sider>
+        <Content>View1</Content>
+        <Content>View2</Content>
+        <Content>View3</Content>
+      </Sider>
+      <Layout>
+        <Content>View4</Content>
+        <Layout>
+          <Content>View5</Content>
+          <Sider>View6</Sider>
+        </Layout>
+      </Layout>
+    </Layout>
   );
-}
+};
 
 export default App;
