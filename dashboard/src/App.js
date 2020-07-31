7dec9ed97cfc13ccdaba8983cd16fc740ab5b579
sapinspys <garri009@fiu.edu>
2020-07-30 21:31:59 -0400

    App displaying single visualization correctly

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
-import { Layout } from "antd";
+import React, { useEffect, useState } from "react";
+import { Layout, Spin } from "antd";
+
+import Plot from "react-plotly.js";
+
 import "./App.less";
 
 const { Sider, Content } = Layout;
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(
+      "http://0.0.0.0:5000/visualization/top10contributors/kubernetes/kubernetes",
+      {
+        method: "GET",
+        headers: {
+          Authorization: process.env.REACT_APP_GH_TOKEN,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log("Success", response);
+        setData(response);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log("Error:", err);
+      });
+  }, []);
+
   return (
     <Layout className="screen">
       <Sider>
@@ -15,7 +42,36 @@ const App = () => {
       <Layout>
         <Content>View4</Content>
         <Layout>
-          <Content>View5</Content>
+          <Content>
+            {isLoading ? 
+              <Spin /> : 
+              <Plot 
+                data={[
+                  {
+                    x: data.user,
+                    y: data.total_commits,
+                    type: "scatter",
+                    mode: "markers",
+                    marker: {
+                      size: 20, 
+                      color: data.followers,showscale: true,
+                    }
+                  }
+                ]}
+                layout={
+                  {
+                    width: "100%",
+                    title: "Top 10 All-Time Contributors",
+                    xaxis: {
+                      tickangle: 45
+                    },
+                    yaxis: {
+                      title: "Total Commits"
+                    }
+                  }
+                }
+              />}
+            </Content>
           <Sider>View6</Sider>
         </Layout>
       </Layout>
