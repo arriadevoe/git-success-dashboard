3e811a01b5c60fd8a44c52cf616b2a9d1bc4dc25
sapinspys <garri009@fiu.edu>
2020-08-01 08:53:46 -0400

    Added no data loaded messages

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Layout, Spin } from "antd";
+import { Layout, Spin, Empty } from "antd";
 
 import Plot from "react-plotly.js";
 
@@ -8,7 +8,7 @@ import "./App.less";
 const { Sider, Content } = Layout;
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState({});
 
   // useEffect(() => {
@@ -34,47 +34,57 @@ const App = () => {
 
   return (
     <Layout className="screen">
-      <Sider>
+      <Sider className="side-menu">
         <Content className="content">View1</Content>
         <Content className="content">View2</Content>
         <Content className="content">View3</Content>
       </Sider>
-      <Content className="content">View4</Content>
-      <Content className="content">View5</Content>
       <Layout>
         <Content className="content">
-          {isLoading ? (
-            <Spin />
-          ) : (
-            <Plot
-              data={[
-                {
-                  x: data.user,
-                  y: data.total_commits,
-                  type: "scatter",
-                  mode: "markers",
-                  marker: {
-                    size: 20,
-                    color: data.followers,
-                    showscale: true,
-                  },
-                },
-              ]}
-              layout={{
-                width: "100%",
-                title: "Top 10 All-Time Contributors",
-                xaxis: {
-                  tickangle: 45,
-                },
-                yaxis: {
-                  title: "Total Commits",
-                },
-              }}
-            />
-          )}
+          <Empty description="No Visualization Loaded" />
         </Content>
-        <Content className="content">View6</Content>
-        <Sider>
+        <Content className="content">
+          <Empty description="No Visualization Loaded" />
+        </Content>
+      </Layout>
+      <Layout>
+        <Layout>
+          <Content className="content">
+            {isLoading ? (
+              <Spin />
+            ) : (
+              <Plot
+                data={[
+                  {
+                    x: data.user,
+                    y: data.total_commits,
+                    type: "scatter",
+                    mode: "markers",
+                    marker: {
+                      size: 20,
+                      color: data.followers,
+                      showscale: true,
+                    },
+                  },
+                ]}
+                layout={{
+                  width: "100%",
+                  title: "Top 10 All-Time Contributors",
+                  xaxis: {
+                    tickangle: 45,
+                  },
+                  yaxis: {
+                    title: "Total Commits",
+                  },
+                }}
+              />
+            )}
+          </Content>
+          <Content className="content">
+            <Empty description="No Visualization Loaded" />
+          </Content>
+        </Layout>
+        <Sider className="side-menu">
           <Content className="content">View7</Content>
           <Content className="content">View8</Content>
           <Content className="content">View9</Content>
