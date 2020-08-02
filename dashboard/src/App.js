d4639bae6b00c297302fe6518daa7f20a724d9d1
sapinspys <garri009@fiu.edu>
2020-08-01 23:50:35 -0400

    Updated main app file to use visualizations

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,113 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { Layout, Spin, Empty, Menu, Dropdown } from "antd";
-import { DownOutlined } from "@ant-design/icons";
+import { Layout, Spin } from "antd";
 
-import Plot from "react-plotly.js";
+import Visualization from "./components/Visualization";
 
 import "./App.less";
 
 const { Sider, Content } = Layout;
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState({});
-
-  // useEffect(() => {
-  //   fetch(
-  //     "http://0.0.0.0:5000/visualization/top10contributors/kubernetes/kubernetes",
-  //     {
-  //       method: "GET",
-  //       headers: {
-  //         Authorization: process.env.REACT_APP_GH_TOKEN,
-  //       },
-  //     }
-  //   )
-  //     .then((response) => response.json())
-  //     .then((response) => {
-  //       console.log("Success", response);
-  //       setData(response);
-  //       setIsLoading(false);
-  //     })
-  //     .catch((err) => {
-  //       console.log("Error:", err);
-  //     });
-  // }, []);
-
-  const chooseVisualization = ({ key }) => {
-    console.log(`Click on item ${key}`);
-  };
-
-  const menu = (
-    <Menu onClick={chooseVisualization}>
-      <Menu.Item key="1">Top 10 All-Time Contributors</Menu.Item>
-      <Menu.Item key="2">2nd memu item</Menu.Item>
-    </Menu>
-  );
-
   return (
     <Layout className="screen">
       <Sider className="side-menu">
-        <Content className="menu-item">View1</Content>
-        <Content className="menu-item">View2</Content>
-        <Content className="menu-item">View3</Content>
+        <Content className="menu-item">Owner</Content>
+        <Content className="menu-item">Repository</Content>
+        <Content className="menu-item">Search</Content>
       </Sider>
       <Layout>
         <Content className="content">
-          <Dropdown overlay={menu}>
-            <a
-              className="ant-dropdown-link"
-              onClick={(e) => e.preventDefault()}
-            >
-              Choose Visualization <DownOutlined />
-            </a>
-          </Dropdown>
-          {isLoading ? (
-            <Spin />
-          ) : (
-            <Plot
-              data={[
-                {
-                  x: data.user,
-                  y: data.total_commits,
-                  type: "scatter",
-                  mode: "markers",
-                  marker: {
-                    size: 20,
-                    color: data.followers,
-                    showscale: true,
-                  },
-                },
-              ]}
-              layout={{
-                width: "100%",
-                title: "Top 10 All-Time Contributors",
-                xaxis: {
-                  tickangle: 45,
-                },
-                yaxis: {
-                  title: "Total Commits",
-                },
-              }}
-            />
-          )}
+          <Visualization name="top10contributors" />
         </Content>
         <Content className="content">
-          <Empty description="No Visualization Loaded" />
+          <Visualization name="top10contributors" />
         </Content>
       </Layout>
       <Layout>
         <Layout>
           <Content className="content">
-            <Empty description="No Visualization Loaded" />
+            <Visualization name="top10contributors" />
           </Content>
           <Content className="content">
-            <Empty description="No Visualization Loaded" />
+            <Visualization name="top10contributors" />
           </Content>
         </Layout>
         <Sider className="side-menu">
-          <Content className="menu-item">View7</Content>
-          <Content className="menu-item">View8</Content>
-          <Content className="menu-item">View9</Content>
+          <Content className="menu-item">Owner</Content>
+          <Content className="menu-item">Repository</Content>
+          <Content className="menu-item">Search</Content>
         </Sider>
       </Layout>
     </Layout>
