6ab639f34cf6d25e624ea37dd667794c649f81f8
sapinspys <garri009@fiu.edu>
2020-08-06 15:26:04 -0400

    Added new visualization type

---
+++ dashboard/src/components/YearlyCodeFrequency.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import Plot from "react-plotly.js";
+
+const YearlyCodeFrequency = (props) => {
+
+  return (
+    <Plot
+      data={[
+        {
+          x: props.data.week,
+          y: props.data.additions,
+          type: "bar",
+          "marker.color": "green"
+        },
+        {
+          x: props.data.week,
+          y: props.data.deletions,
+          type: "bar",
+          "marker.color": "red"
+        },
+      ]}
+      layout={{
+        width: "100%",
+        title: `Yearly Code Frequency: ${props.repo}`,
+        barmode: "overlay"
+      }}
+    />
+  );
+};
+
+export default YearlyCodeFrequency;
