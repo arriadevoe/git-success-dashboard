4ed47169d2aabcad1b2db0f4d41cb4d8289dadc4
sapinspys <garri009@fiu.edu>
2020-08-18 22:48:36 -0400

    Created new daily commits plot component

--- dashboard/src/components/DailyCommitsPlot.js
+++ dashboard/src/components/DailyCommitsPlot.js
@@ -2,20 +2,20 @@ import React from "react";
 
 import Plot from "react-plotly.js";
 
-const YearlyCommitActivityPlot = (props) => {
+const DailyCommitsPlot = (props) => {
 
   return (
     <Plot
       data={[
         {
-          x: props.data.week,
-          y: props.data.total_commits,
+          x: props.data.day,
+          y: props.data.commits,
           type: "bar",
         },
       ]}
       layout={{
         width: "100%",
-        title: `Yearly Commit Activity: ${props.repo}`,
+        title: `Last Week's Daily Commits: ${props.repo}`,
         yaxis: {
           title: "Total Commits",
         },
@@ -24,4 +24,4 @@ const YearlyCommitActivityPlot = (props) => {
   );
 };
 
-export default YearlyCommitActivityPlot;
+export default DailyCommitsPlot;
